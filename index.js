b897f729c49be54fe8409e1a3830932bf01678fe
Mercy <owinomercy983@gmail.com>
2025-03-07 08:47:10 +0300

    acting on events lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+
+function moveDodgerLeft() {
+    const left = parseInt(dodger.style.left.replace("px", ""), 10);
+    if (left > 0) {  // Prevents moving out of bounds
+        dodger.style.left = `${left - 10}px`
+    }
+}
+
+
+function moveDodgerRight() {
+    const left = parseInt(dodger.style.left.replace("px", ""), 10);
+    if (left < 360) {  
+        dodger.style.left = `${left + 10}px`;
+    }
+}
+
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
